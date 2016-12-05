659ad7fc32ef994ba3b4ee7bbab1ecfb664f8fa9
NycuRO <nghergu@yahoo.com>
2016-12-05 19:55:15 +0200

    Add files

---
+++ src/main/java/cn/nukkit/network/protocol/FullChunkDataPacket.java
@@ -0,0 +1,36 @@
+package cn.nukkit.network.protocol;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class FullChunkDataPacket extends DataPacket {
+    public static final byte NETWORK_ID = ProtocolInfo.FULL_CHUNK_DATA_PACKET;
+
+    public static final byte ORDER_COLUMNS = 0;
+    public static final byte ORDER_LAYERED = 1;
+
+    @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+    public int chunkX;
+    public int chunkZ;
+    public byte order = ORDER_COLUMNS;
+    public byte[] data;
+
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void encode() {
+        this.reset();
+        this.putVarInt(this.chunkX);
+        this.putVarInt(this.chunkZ);
+        this.putByte(this.order);
+        this.putByteArray(this.data);
+    }
+}
